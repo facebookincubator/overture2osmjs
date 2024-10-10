4989bb3f3fc7056de200e8df9a75c9025e4ac62c
Petar Dosev <pdosev@gmail.com>
2024-10-09 20:13:35 -0400

    Simple tag exporter

--- o2o-cli.js
+++ o2o-cli.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import * as fs from 'node:fs/promises';
 
-import {overtureToOsmData} from "./src/overture2osm.js";
+import {overtureToOSMData} from "./src/overture2osm.js";
 import minimist from 'minimist';
 
 const argv = minimist(process.argv.slice(2));
-console.log(argv);
 if (argv.help) {console.log(`
 Overture to OSM Data Converter
 
@@ -20,7 +19,10 @@ if (!filename) {console.log("o2o-cli: Please enter file name") }
 else {
     try {
         const data = JSON.parse(await fs.readFile(filename, {encoding: 'utf-8'}));
-        overtureToOsmData(data);
+        for (const feature of data.features) {
+            // just print to console for now
+            console.log(overtureToOSMData(feature));
+        }
     } catch(error) {
         console.error('File error:', error.message);
     }
--- src/overture2osm.js
+++ src/overture2osm.js
@@ -1,7 +1,77 @@
-function overtureToOsmData(overtureData) {
-  for (const feature of overtureData.features) {
-    console.log(`name=${feature.properties.names.primary}`);
+import osm_tag_dict from "./overture2osm.json" with { type: "json" };
+
+/**
+ * Looks up OSM tags corresponding to Overture category from embedded dictionary
+ *
+ * @param {string} overtureCategory - The name of the category.
+ *
+ * @returns {?object} An object where key/value pairs correspond to OSM tag key/values,
+ * or null if the category isn't found in the embedded dictionary.
+ *
+ * Both the keys and values are strings, including namespaced keys and semicolon-separated values.
+ */
+function getOSMCategoryTags (overtureCategory) {
+  if (osm_tag_dict[overtureCategory] === undefined) {
+    console.warn(`Category ${overtureCategory} not found in dictionary!`);
+    return null;
   }
+  return osm_tag_dict[overtureCategory].osm_tags;
 }
 
-export {overtureToOsmData};
+/**
+ * Converts an Overture Place-type geojson object to OSM tags.
+ *
+ * @param {object} feature - An Overture Place-type object.
+ * See https://docs.overturemaps.org/schema/reference/places/place/
+ *
+ * @returns {object} An object containing key/value pairs corresponding to OSM tags.
+ * Both the key and the value are strings.
+ */
+function overtureToOSMData (feature) {
+  const props = feature.properties;
+  let osm_tags = {};
+
+  // either null or the primary category
+  // optional chaining operator (the ?.) is supported in post-2020 browsers. what versions are we targeting?
+  const primary_category = props?.categories?.primary;
+
+  // we need to skip the category of 'structure_and_geography' because it is too vague to convert without context
+  if (primary_category && primary_category !== "structure_and_geography") {
+    Object.assign(osm_tags, getOSMCategoryTags(primary_category));
+  } else {
+    console.warn(`No primary category in feature with ID ${props.id}`);
+  }
+
+  if (props?.names?.primary) {
+    osm_tags.name = props.names.primary;
+  } else {
+    console.warn(`No primary name in feature with ID ${props.id}`);
+  }
+
+  if (props?.brand?.names?.primary) {
+    osm_tags.brand = props.brand.names.primary;
+  }
+
+  if (props?.phones) osm_tags['contact:phone'] = props.phones.join(';');
+  if (props?.websites) osm_tags['contact:website'] = props.websites.join(';');
+  if (props?.emails) osm_tags['contact:email'] = props.emails.join(';');
+
+  if (props?.socials) {
+    for (const url of props.socials) {
+      // assumes no subdomains or weird urls, and no duplicate links
+      const website_name = url.split('.')[1];
+      osm_tags[`contact:${website_name}`] = url;
+    }
+  }
+
+  if (props?.sources) {
+    for (const source of props.sources) {
+      if (source.record_id) osm_tags.source = `Overture/${source.dataset}/${source.record_id}`;
+      else osm_tags.source = `Overture/${source.dataset}`;
+    }
+  }
+
+  return osm_tags;
+}
+
+export {getOSMCategoryTags, overtureToOSMData};
