47a4bd9dd92a0502d17e50993051e32ae17df573
Petar Dosev <pdosev@gmail.com>
2024-11-19 15:07:59 -0500

    Add a coordinate precision heuristic

--- src/fetchAddresses.js
+++ src/fetchAddresses.js
@@ -1,4 +1,22 @@
 /**
+ * Returns the count of fractional digits of the number
+ * Ex: 1.234 -> 3, 100 -> 0, 3.14159 -> 5, etc.
+ * Prone to floating point issues, like 0.44399999999999999 -> 3
+ * We don't care since we only use this as a heuristic
+ *
+ * @param {number} num - Number with fractional digits to be counted
+ *
+ * @returns {number} The count of fractional digits
+ */
+
+function precision(num) {
+  if (!isFinite(num) || !(typeof num == "number")) return 0;
+  let exp = 1, precision = 0;
+  while (Math.trunc(num * exp) / exp !== num) { exp *= 10; precision++; }
+  return precision;
+}
+
+/**
  * Looks up address information from Nominatim
  *
  * @param {number} lat - Latitude of the address to look up
@@ -27,9 +45,16 @@ async function getNominatimAddress(lat, lon) {
   const geocoding = data.features[0].properties.geocoding;
   const address = {};
 
-  if (geocoding.housenumber) address["addr:housenumber"] = geocoding.housenumber;
-  if (geocoding.street) address["addr:street"] = geocoding.street;
-  if (geocoding.postcode) address["addr:postcode"] = geocoding.postcode;
+  // If either latitude and longitude have less than 4 digits of precision, we count them as imprecise.
+  // This is about 100m of error at the equator, and in testing
+  const lowPrecisionCoords = Math.min(precision(lat), precision(lon)) < 4;
+
+  if (!lowPrecisionCoords) {
+    if (geocoding.housenumber) address["addr:housenumber"] = geocoding.housenumber;
+    if (geocoding.street) address["addr:street"] = geocoding.street;
+    if (geocoding.postcode) address["addr:postcode"] = geocoding.postcode;
+  }
+
   if (geocoding.city) address["addr:city"] = geocoding.city;
   if (geocoding.country) address["addr:country"] = geocoding.country;
   return address;
