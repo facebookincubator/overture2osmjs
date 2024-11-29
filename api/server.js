7bd930810bfa18fb1add170c7f0b310fa305a8b7
Petar Dosev <pdosev@gmail.com>
2024-11-29 18:14:37 -0500

    Add copyright info before going public

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import express from 'express';
 import { validateGeoJSON } from '../validate.js';
 import appRoute from "./routes/app.route.js";
